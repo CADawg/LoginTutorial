946ee11b17ada6f0623862a4c1f3184a1198fe85
Conor  H <conor.howland@outlook.com>
2018-01-14 18:50:46 +0000

    Add Index.PHP

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php session_start(); ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Sign Up/Register</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
+</head>
+<a href="login.php"><button class="w3-btn w3-round-large w3-blue w3-medium w3-padding-large">Login</button></a>
+<a href="register.php"><button class="w3-btn w3-round-large w3-blue w3-medium w3-padding-large">Register</button></a>
+<body>
+</body>
+</html>
